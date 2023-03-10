fc67b36501d20c7dd85c668bb1930d2f76fb53bb
Thiago Alberto <71457360+Thiago1alberto@users.noreply.github.com>
2023-03-10 16:20:41 -0300

    Update JogadorAleatorio.java

--- Jogo da Velha/src/jogadores/JogadorAleatorio.java
+++ Jogo da Velha/src/jogadores/JogadorAleatorio.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jogadores;
 
 import java.util.Random;
 import jogo.Tabuleiro;
 
-/**
- *
- * @author tarci
- */
 public class JogadorAleatorio extends Jogador{
 
     public JogadorAleatorio(String nome) {
