2e8f4bf753951de5f69aa1581d4dafcd0b6aa68d
Thiago Alberto <71457360+Thiago1alberto@users.noreply.github.com>
2023-03-10 16:20:20 -0300

    Update Jogador.java

--- Jogo da Velha/src/jogadores/Jogador.java
+++ Jogo da Velha/src/jogadores/Jogador.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jogadores;
 
 import jogo.Tabuleiro;
 
-/**
- *
- * @author tarci
- */
+
 public abstract class Jogador {
     private int simbolo;
     private String nome;
