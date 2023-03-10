3465db007ce267a2fe26fb3f7a73a90d2eca2cf3
Thiago Alberto <71457360+Thiago1alberto@users.noreply.github.com>
2023-03-10 16:21:05 -0300

    Update JogadorLinhaColuna.java

--- Jogo da Velha/src/jogadores/JogadorLinhaColuna.java
+++ Jogo da Velha/src/jogadores/JogadorLinhaColuna.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jogadores;
 
 import jogo.Tabuleiro;
 
-/**
- *
- * @author tarci
- */
 public class JogadorLinhaColuna extends Jogador{
 
     public JogadorLinhaColuna(String nome) {
