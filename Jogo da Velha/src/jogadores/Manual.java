a969bf9a09605fb489084c7e66289b2b7076159e
Thiago Alberto <71457360+Thiago1alberto@users.noreply.github.com>
2023-03-10 16:21:24 -0300

    Update Manual.java

--- Jogo da Velha/src/jogadores/Manual.java
+++ Jogo da Velha/src/jogadores/Manual.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jogadores;
 
 import static java.lang.System.in;
 import java.util.Scanner;
 import jogo.Tabuleiro;
 
-/**
- *
- * @author tarci
- */
+
 public class Manual extends Jogador{
 
     public Manual(String nome) {
